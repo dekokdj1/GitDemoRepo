24813826f4bed1ccb124b7c6ec74a1bb12b67c47
Dyllon <dyllon.dekok1@gmail.com>
2022-10-27 12:15:28 -0400

    commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    //comment for feature-x branch
+    //comment for feature-y branch
     public IActionResult Index()
     {
         return View();
